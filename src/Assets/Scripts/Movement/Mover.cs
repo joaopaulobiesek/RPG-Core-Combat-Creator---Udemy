734a4f87c173c0acdfb65860a36f3691db602a18
Joao Paulo <joaopaulo@wiesoo.com>
2024-01-23 16:40:40 -0300

    Separating The Control Layer

---
+++ src/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Update()
+    {        
+        UpdateAnimator();
+    }    
+
+    public void MoveTo(Vector3 destination)
+    {
+        GetComponent<NavMeshAgent>().destination = destination;
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+}
